1a287aab7df88d533a7432bf8b8cd4c0b948a05d
Paweł Białas <pawel.bialas@empyrean.games>
2017-08-09 14:39:00 +0200

    roadlevel

---
+++ Assets/Scripts/Road/RoadFragment.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class RoadFragment : MonoBehaviour {
+    public Vector3 positionDelta;
+}
---
+++ Assets/Scripts/Road/RoadSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour {
+    [SerializeField] Transform roadHolder;
+    [SerializeField] List<RoadFragment> roadFragments;
+    [SerializeField] int roadLength = 20;
+    [SerializeField] RoadFragment safeFragment;
+    [SerializeField] int safeAmount = 3;
+
+    Vector3 currentPosition = Vector3.zero;
+
+    void Awake() {
+        SpawnRoad();
+    }
+
+    void SpawnRoad() {
+        var i = 0;
+        for (; i < safeAmount; i++)
+            SpawnFragment(currentPosition, safeFragment);
+        for (; i < roadLength; i++)
+            SpawnFragment(currentPosition, GetRandomFragment());
+    }
+
+    void SpawnFragment(Vector3 position, RoadFragment fragment) {
+        var fragmentObject = Instantiate(fragment.gameObject);
+        SetupFragmentTransform(position, fragmentObject);
+        UpdateTransformData(fragment);
+    }
+
+    void UpdateTransformData(RoadFragment fragment) {
+        currentPosition += fragment.positionDelta;
+    }
+
+    void SetupFragmentTransform(Vector3 position, GameObject fragmentObject) {
+        fragmentObject.transform.parent = roadHolder;
+        fragmentObject.transform.localPosition = position;
+        fragmentObject.transform.rotation = Quaternion.identity;
+    }
+
+    RoadFragment GetRandomFragment() {
+        var fragmentIndex = Random.Range(0, roadFragments.Count);
+        var fragment = roadFragments[fragmentIndex];
+        return fragment;
+    }
+}
