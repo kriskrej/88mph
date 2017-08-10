e184e9bd412cbe50a9398e4efc5398191d8cf067
Paweł Białas <pawel.bialas@empyrean.games>
2017-08-10 13:53:42 +0200

    race scripts

---
+++ Assets/Scripts/Track/TrackCheckpoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TrackCheckpoint : MonoBehaviour {
+    [SerializeField] int checkpointNumber;
+
+    TrackController trackController;
+
+    void Awake() {
+        trackController = FindObjectOfType<TrackController>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (ColliderIsPlayer(other))
+            trackController.ReportEntrance(checkpointNumber);
+    }
+
+    bool ColliderIsPlayer(Collider other) {
+        return other.CompareTag("Player");
+    }
+}
---
+++ Assets/Scripts/Track/TrackController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackController : MonoBehaviour {
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject gate;
+    [SerializeField] Text statusText;
+    [SerializeField] int numberOfCheckpoints;
+    [SerializeField] int numberOfLaps = 4;
+
+    int currentCheckpoint;
+    int currentLap;
+    float startTime;
+
+    void Awake() {
+        currentCheckpoint = 0;
+        currentLap = 0;
+    }
+
+    void Start() {
+        Invoke("StartRace", 3f);
+        statusText.text = "";
+    }
+
+    public void ReportEntrance(int checkpointNumber) {
+        if (EnteredRightCheckpoint(checkpointNumber))
+            SwitchToNextCheckpoint();
+    }
+
+    void SwitchToNextCheckpoint() {
+        currentCheckpoint += 1;
+        if (LapIsFinished())
+            SwitchToNextLap();
+    }
+
+    void SwitchToNextLap() {
+        currentLap += 1;
+        currentCheckpoint = 0;
+        if (RaceIsFinished())
+            FinishRace();
+        else
+            statusText.text = string.Format("Lap {0}", currentLap + 1);
+    }
+
+    void FinishRace() {
+        statusText.text = string.Format("Time: {0:0.00}", Time.time - startTime);
+        gate.SetActive(false);
+    }
+
+    bool RaceIsFinished() {
+        return currentLap == numberOfLaps;
+    }
+
+    bool LapIsFinished() {
+        return currentCheckpoint == numberOfCheckpoints;
+    }
+
+    bool EnteredRightCheckpoint(int checkpointNumber) {
+        return checkpointNumber == currentCheckpoint;
+    }
+
+    void StartRace() {
+        start.SetActive(false);
+        startTime = Time.time;
+        statusText.text = "Lap 1";
+    }
+}
