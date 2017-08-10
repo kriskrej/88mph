7b2a8fb0ee14b67dcd2adf769ffca93c317fb102
Jakub Bujas <kuba.bujas@empyrean.games>
2017-08-10 14:14:01 +0200

    Add mark of jump script and prefab.

---
+++ Assets/Scripts/MarkOfJump.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkOfJump : MonoBehaviour {
+
+    public Rigidbody measuredBody;
+    [SerializeField] private float jumpFactor = 1.2f;
+
+    void OnTriggerEnter() {
+        measuredBody.velocity = measuredBody.velocity + new Vector3(0,jumpFactor,0);
+    }
+	
+}
