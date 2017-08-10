9d0d4432efdb7da3aad0ae41228e77ca1bf92710
Jakub Bujas <kuba.bujas@empyrean.games>
2017-08-10 13:27:34 +0200

    Add mark of speed script and prefab.

---
+++ Assets/Scripts/MakOfSpeed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakOfSpeed : MonoBehaviour {
+
+    public Rigidbody measuredBody;
+    [SerializeField] private float speedFactor = 1.2f;
+
+    void OnTriggerEnter() {
+        measuredBody.velocity = measuredBody.velocity*speedFactor;
+    }
+	
+}
