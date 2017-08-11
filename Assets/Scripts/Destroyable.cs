bcae0466d1096847f599f1f86c9b603a725f3000
Paweł Białas <pawel.bialas@empyrean.games>
2017-08-11 14:59:53 +0200

    MountainLevel1 - destroyable wall prototype

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour {
+    [SerializeField] List<GameObject> destroyableFragments;
+    [SerializeField] float initialDurability;
+
+    float durability;
+
+    void Awake() {
+        durability = initialDurability;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        durability -= collision.impulse.magnitude;
+        if (durability < 0f) {
+            foreach (var fragment in destroyableFragments) {
+                Destroy(fragment.GetComponent<FixedJoint>());
+                fragment.GetComponent<Rigidbody>().WakeUp();    
+            }
+            GetComponent<Collider>().enabled = false;
+            GetComponent<Renderer>().enabled = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Editor/DestroyableEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Destroyable))]
+public class DestroyableEditor : Editor {
+    SerializedProperty destroyableFragments;
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Add all children"))
+            AddAllChildren();
+    }
+
+    void AddAllChildren() {
+        destroyableFragments = serializedObject.FindProperty("destroyableFragments");
+        foreach (var joint in GetChildren())
+            AddDestroyableFragment(joint);
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void AddDestroyableFragment(Object joint) {
+        var index = destroyableFragments.arraySize;
+        destroyableFragments.InsertArrayElementAtIndex(index);
+        destroyableFragments.GetArrayElementAtIndex(index).objectReferenceValue = joint;
+    }
+
+    List<GameObject> GetChildren() {
+        return ((Destroyable) target).GetComponentsInChildren<FixedJoint>()
+            .Select(x => x.gameObject)
+            .ToList();
+    }
+}
